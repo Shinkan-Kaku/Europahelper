1eb13eb947f1d2cb3f3b9b850b6b175d4fa6b1ef
lost1212 <a133022001@gmail.com>
2018-02-11 12:06:27 +0900

    加入專案檔案。

---
+++ Europahelper/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Europahelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bnTargetChoice = new System.Windows.Forms.Button();
+            this.AdrsCdtChkBox = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bnTargetChoice);
+            this.panel1.Controls.Add(this.AdrsCdtChkBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // bnTargetChoice
+            // 
+            this.bnTargetChoice.Location = new System.Drawing.Point(60, 3);
+            this.bnTargetChoice.Name = "bnTargetChoice";
+            this.bnTargetChoice.Size = new System.Drawing.Size(75, 23);
+            this.bnTargetChoice.TabIndex = 0;
+            this.bnTargetChoice.Text = "Target";
+            this.bnTargetChoice.UseVisualStyleBackColor = true;
+            this.bnTargetChoice.Click += new System.EventHandler(this.BnTargetChoice_Click);
+            // 
+            // AdrsCdtChkBox
+            // 
+            this.AdrsCdtChkBox.AutoSize = true;
+            this.AdrsCdtChkBox.Enabled = false;
+            this.AdrsCdtChkBox.Location = new System.Drawing.Point(141, 4);
+            this.AdrsCdtChkBox.Name = "AdrsCdtChkBox";
+            this.AdrsCdtChkBox.Size = new System.Drawing.Size(15, 14);
+            this.AdrsCdtChkBox.TabIndex = 0;
+            this.AdrsCdtChkBox.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(260, 201);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(252, 175);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "GetDataList";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "countries",
+            "provinces"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(252, 175);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bnTargetChoice;
+        private System.Windows.Forms.CheckBox AdrsCdtChkBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ Europahelper/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Europahelper
+{
+    public partial class Form1 : Form
+    {
+        Thread AddressCdtListener;
+        bool needToClose;
+        public Form1()
+        {
+            needToClose = false;
+            AddressCdtListener = new Thread(AddressCdtMethod);
+            InitializeComponent();
+        }
+
+        private void AddressCdtMethod()
+        {
+            while(needToClose!=false)
+            {
+                 
+            }
+        }
+
+        private void IntermeneUI()
+        {
+
+        }
+
+        private string RequestUIValue()
+        {
+            return "";
+        }
+
+
+
+        private void BnTargetChoice_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, EventArgs e)
+        {
+            needToClose = true;
+            AddressCdtListener.Abort();
+        }
+    }
+}
